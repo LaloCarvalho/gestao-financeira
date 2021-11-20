46dd2f915c712c5d14dd756dbcb20780e004e5a3
LaloCarvalho <eduardocdiogenes@gmail.com>
2021-11-20 15:20:50 -0300

    Finalização da tela de login

---
+++ src/components/textBox/styles.ts
@@ -0,0 +1,6 @@
+import { TextField } from "@mui/material";
+import styled from "styled-components";
+
+export const TextBox = styled(TextField)`
+  margin: auto;
+`;
--- src/views/signIn/index.tsx
+++ src/views/signIn/index.tsx
@@ -1,11 +1,46 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import Button from "../../components/button";
+import TextBox from "../../components/textBox";
+import PasswordBox from "../../components/passwordBox";
+import { Container, DivForm, H1 } from "./styles";
 
 const SignIn: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>, text: string) => {
+    console.log(text);
+  };
+
   return (
-      <Container>
-        <h1 className="container-fluid text-center text-md-left">Login em contrução</h1>
-      </Container>
+    <Container>
+      <div className="container-fluid text-center text-md-left">
+        <Row>
+          <Col md={3}></Col>
+          <DivForm className="col-md-6 col-md-offset-1">
+            <H1>Faça seu login</H1>
+            <TextBox
+              width={440}
+              id="outlined-basic-email"
+              label="Email"
+              onChange={(event: any) => setEmail(event.target.value)}
+            >
+            </TextBox><br/>
+            <PasswordBox
+              id="outlined-basic-password"
+              onChange={(event: any) => setPassword(event.target.value)}
+              label="Senha"
+            >
+            </PasswordBox><br/>
+            <Button onClick={(e: any) => { handleClick(e, "clicked"); }}>
+              Login
+            </Button>
+          </DivForm>
+          <Col md={3}></Col>
+        </Row>
+      </div>
+    </Container>
   );
 };
 
