4195a1c8c43fff497c448d83d59820fde90509b2
LaloCarvalho <eduardocdiogenes@gmail.com>
2021-11-26 10:48:57 -0300

    inicio do layout da home

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,11 +1,83 @@
 import React from "react";
-import { Container } from "./styles";
+import { Carousel, Col, Container, Row } from "react-bootstrap";
+import { Con, DivImg, Img, ImgCarousel, row } from "./styles";
+import imagem1 from "./assets/imagem1.jpg";
+import imagem2 from "./assets/imagem2.png";
+import imagem3 from "./assets/imagem3.jpg";
 
 const Home: React.FC = () => {
   return (
-      <Container>
-        <h1 className="container-fluid text-center text-md-left">Home em construção</h1>
-      </Container>
+    <Container style={Con}>
+      <Carousel fade>
+        <Carousel.Item interval={1000}>
+          <ImgCarousel
+            src={imagem1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <ImgCarousel
+            src={imagem2}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <ImgCarousel
+            src={imagem3}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      <DivImg >
+        <Img
+          src={imagem3}
+          alt="Third slide"
+        />
+        </DivImg>
+        <Row >        
+        <Col md={4}>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Col>
+        <Col md={4}>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Col>
+        <Col md={2}>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Col>
+        <Col md={2}>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Col>
+        </Row>
+    </Container>
   );
 };
 
--- src/views/home/styles.ts
+++ src/views/home/styles.ts
@@ -3,3 +3,26 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin: 66px;
 `;
+
+export const ImgCarousel = styled.img`
+  width: 100%;
+  height: 650px;
+  text-align: center;
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  height: 400px;
+  text-align: center;
+`;
+
+export const DivImg = styled.div`
+  width: 50%;
+  padding-top: 100px;
+  height: 400px;
+  text-align: left;
+  background-color: 'rgba(95, 97, 98, 0.1)'
+`;
+
+export const Con = { paddingLeft: 160, paddingRight: 175 };
+export const row = { paddingTop: 100, backgroundColor: 'rgba(95, 97, 98, 0.1)'};
